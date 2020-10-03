1711aba6b128d403b1a5b4e7953b8bb05a86458b
Russell Steadman <russellsteadman@gmail.com>
2020-10-03 08:53:18 -0400

    initial website

---
+++ precache-manifest.8c6fdd0412418f6e3cf35ec20b1f24bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31efcb1205f135cfbfba9a797d1c45fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a47625781a51841aee3b",
+    "url": "/static/css/main.8b99a572.chunk.css"
+  },
+  {
+    "revision": "ff8fd54e1647e04bdce1",
+    "url": "/static/js/2.158f9726.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.158f9726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47625781a51841aee3b",
+    "url": "/static/js/main.86e3e986.chunk.js"
+  },
+  {
+    "revision": "c15c5c07a687f4b5e55c",
+    "url": "/static/js/runtime-main.c7f4dc50.js"
+  }
+]);
